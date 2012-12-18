f3e815f3876254303691d4372b58556cacc0e693
Andreas Rain <andreas.rain@uni-konstanz.de>
2012-12-18 13:04:16 +0100

    [MOD] Missing migration and schema definition of the remember token!

---
+++ db/migrate/20121218120307_add_remember_token_column.rb
@@ -0,0 +1,5 @@
+class AddRememberTokenColumn < ActiveRecord::Migration
+  def change
+    add_column :users, :remember_token, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -427,6 +427,7 @@ ActiveRecord::Schema.define(:version => 20121211161953) do
     t.string   "email",                                             :default => "",    :null => false
     t.string   "encrypted_password",                                :default => "",    :null => false
     t.string   "invitation_token",                   :limit => 60
+    t.string   "remember_token"
     t.datetime "invitation_sent_at"
     t.string   "reset_password_token"
     t.datetime "remember_created_at"
